550ebf738a72615200763b932b49d56bb6db580a
raffoluciano <42227702+raffoluciano@users.noreply.github.com>
2022-08-31 20:15:34 -0300

    serviciosusuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { getUsuarios } from './services/usuarioService';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsuarios().then(us => setUsers(us));
+  },[])
+  
+  console.log(users)
+
   return (
     <div className="App">
       <header className="App-header">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link" href="#">Inicio</a>
-              <a class="nav-link" href="paquetes.html">Paquetes</a>
-              <a class="nav-link" href="ofertas.html">Ofertas</a>
-              <a class="nav-link" href="contacto.html">Contacto</a>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link" href="#">Inicio</a>
+              <a className="nav-link" href="paquetes.html">Paquetes</a>
+              <a className="nav-link" href="ofertas.html">Ofertas</a>
+              <a className="nav-link" href="contacto.html">Contacto</a>
             </div>
           </div>
         </div>
       </nav>
       </header>
-    </div>
 
-    
+      <ul>
+        {
+          users.map(user => (
+            <li key={user.dni}>{user.nombre}</li>)
+          )
+        }
+      </ul>
+
+    </div> 
   )
 
 }
