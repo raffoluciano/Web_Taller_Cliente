bbfc293ae9771b1dda8533c4a0b13dcaabbc3965
raffoluciano <42227702+raffoluciano@users.noreply.github.com>
2022-09-07 19:05:29 -0300

    coso

---
+++ src/services/usuarioService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const url = "http://localhost:4000/User/users";
+
+// export const getUsuarios = async () => {
+//     const resp = await fetch(url)
+//     const users = await resp.json();
+//     return users;
+// }
+
+
+export const getUsuarios = async () => {
+     const response = await axios.get(url)
+     return response.data
+}
