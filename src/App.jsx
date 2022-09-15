b94b3e0bb365348c9a33f1e563c6b9061ff0a0cb
marimedina <mariescuela@outlook.com>
2022-09-15 20:31:08 -0300

    Creacion de archivo AppRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,15 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { getPackage, getPackageById, getCountry, getUser } from './utils/getdata';
-import Navbar from './components/ui/navbar';
-import Footer from './components/ui/footer';
 import Home from './components/Home';
-import Packages from './components/Packages';
-import { useParams } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 
 function App() {
-
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-    getUser().then(us => setUsers(us));
-  },[])
-  
-
   return (
     <div className="App">
-      <Navbar/>
-      <Packages/>
-      <ul>
-          {
-            users.map(user => (
-              <li key={user.dni}>{user.nombre}</li>) //cambiar el campo dni a id, y el otro que se quiera mostrar p/prueba
-            )
-          }
-      </ul>
-      <Footer/>
-
-     
-
+      <AppRouter/>
     </div> 
   )
-
 }
 
 export default App;
 
-//EJ PARA PROBAR TRAE LOS DATOS DE LAS FUNCIONES SIN PASAR PARAMETROS
-/*
-const [users, setUsers] = useState([]);
-  useEffect(() => {
-    [nombre de la funcion]().then(us => setUsers(us));
-  },[])
 
-  Lo de abajo queda igual
-*/
-
-//EJ PARA PROBAR SI TRAE LOS DATOS DE LAS FUNCIONES QUE RECIBEN UN PARAMETRO ID
-
-/*
-const {id} = useParams();
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    const consultarApi = async() => {
-      setUsers (await getPackageById(id));
-    }
-    consultarApi();
-  },[])
-
-
-  <ul>
-          {
-            users.map(user => (
-              <li key={user.id}>{user.descripcion}</li>) //cambiar el campo dni a id, y el otro que se quiera mostrar p/prueba
-            )
-          }
-  </ul>
-*/
---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useEffect, useState } from 'react';
+import { getPackage, getPackageById, getCountry, getUser } from '../utils/getdata';
+import { useParams } from 'react-router-dom';
+import Navbar from '../components/ui/navbar';
+import Footer from '../components/ui/footer';
+import Packages from '../components/Packages';
+
+const AppRouter = () => {
+
+    return (
+        <div className="AppRouter">
+            <Navbar/>
+            <Packages/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default AppRouter;
+
+//EJ PARA PROBAR TRAE LOS DATOS DE LAS FUNCIONES SIN PASAR PARAMETROS
+/*
+const [users, setUsers] = useState([]);
+  useEffect(() => {
+    [nombre de la funcion]().then(us => setUsers(us));
+  },[])
+
+  Lo de abajo queda igual
+*/
+
+//EJ PARA PROBAR SI TRAE LOS DATOS DE LAS FUNCIONES QUE RECIBEN UN PARAMETRO ID
+
+/*
+  const id = 1
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async() => {
+      setUsers (await getPackageById(id));
+    }
+    consultarApi();
+  },[])
+
+
+  <ul>
+          {
+            users.map(user => (
+              <li key={user.id}>{user.descripcion}</li>) //cambiar el campo dni a id, y el otro que se quiera mostrar p/prueba
+            )
+          }
+  </ul>
+*/
