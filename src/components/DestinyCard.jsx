435166a631d5e64cf56f8c5101eef0b4ef627845
fercafferata <fercafferata@outlook.com>
2023-04-19 18:58:54 -0300

    home

--- src/components/DestinyCard.jsx
+++ src/components/DestinyCard.jsx
@@ -1,17 +1,14 @@
-import { Link } from '@mui/material';
 import logo from '../../public/logoAHORA.png';
 
 export const DestinyCard = ({ destino, id }) => {
   return (
-    <>
+    <>   
           <div className="card">
             <img src={logo} className="card-img-top" alt="..."></img>
             <div className="card-body">
               <h6 className="card-title">{ destino }</h6>
             </div>
-            <Link to={`/package/${id}`}></Link>
           </div>
-          
     </>
   )
 }
